8e5e49331dd013d00633073b33f22d41846ff84a
Rodrigo Gama <121040163+FRgama@users.noreply.github.com>
2025-03-31 00:46:21 -0300

    ajustes

--- b7web-exercicios/src/app/pag02/page.tsx
+++ b7web-exercicios/src/app/pag02/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 
 export default function pag02() {
@@ -33,9 +34,9 @@ export default function pag02() {
 
             <div className="w-3/4 p-6 overflow-hidden mx-auto bg-slate-800 border border-black rounded-xl flex flex-col justify-center items-center md:flex-row">
       
-             <img className="rounded-full w-25 h-25 my-4 object-cover mx-auto md:m-2 " src="https://thefader-res.cloudinary.com/private_images/w_1260,c_limit,f_auto,q_auto:best/J._Cole_Lead_Promo_Image_vwgqi9/j-cole-photo-by-david-peters.jpg" alt="" />
+             <Image width={500} height={500} className="rounded-full w-25 h-25 my-4 object-cover mx-auto md:m-2 " src="/J.-Cole.webp" alt="Imagem do raper J. Cole" />
             <div className="text-center text-lg">
-            <h1 className="text-center text-white">'No such thing in life is better than yours'</h1>
+            <h1 className="text-center text-white">&apos; No such thing in life is better than yours &apos;</h1>
               <h2 className="text-blue-400">Jermaine Lamar Cole</h2>
               <h3 className="text-gray-400">Rapper</h3>
             </div>
